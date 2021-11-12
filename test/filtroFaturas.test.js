f57fe8004462fc298a55144a4339e94cdc9007c6
TiagoPerri <tiagocnsr@gmail.com>
2021-11-12 13:18:07 -0300

    1 ciclo TDD - Refatorando (valor < 2000)

--- test/filtroFaturas.test.js
+++ test/filtroFaturas.test.js
@@ -8,9 +8,12 @@ describe('filtroFaturas', () => {
         let newArr = []
         
         const user1 = filtrofaturas.passaobjCliente('Tiago', '2021-10-07', 'PR');
+        const user2 = filtrofaturas.passaobjCliente('Andre', '2021-08-01', 'AM');
         const fatura1 = filtrofaturas.passaobjFatura('id', 2350, '2021-12-15', user1);
+        const fatura2 = filtrofaturas.passaobjFatura('id2', 1350, '2021-05-03', user2);
 
         newArr = filtrofaturas.colocaFaturainArray(newArr,fatura1)
+        newArr = filtrofaturas.colocaFaturainArray(newArr,fatura2) 
         newArr = filtrofaturas.filtroFatura(newArr);
 
         const sizeArr = newArr.length;
